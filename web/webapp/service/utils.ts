fbc9d82604955353b3aaaaabfc729cf0662f454a
Kamuyin <56199686+NoHaxxJustLags@users.noreply.github.com>
2026-02-27 19:43:51 +0100

    refactor: recreating WebUI

--- web/webapp/service/utils.ts
+++
@@ -1,9 +0,0 @@
-import Component from "../Component";
-import APIService from "../service/APIService";
-import Controller from "sap/ui/core/mvc/Controller";
-
-
-export function getAPIService(controller: Controller): APIService {
-    const component = controller.getOwnerComponent() as Component;
-    return component.getAPIService();
-}
